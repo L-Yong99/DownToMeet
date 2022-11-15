bdfc70c528edba96470532ab581ab3c6816c1a2f
He Xiang Koh <kohhxx6@gmail.com>
2022-11-15 17:57:25 +0800

    added new routes

---
+++ app/controllers/attendances_controller.rb
@@ -0,0 +1,10 @@
+class AttendancesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
+  get 'attendances/index'
+  get 'attendances/show'
+  get 'attendances/destroy'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/destroy'
   devise_for :users
-  root to: "pages#home"
+  root to: "events#index"
+  resources :users do
+    resources :events, only: [:index, :new, :show, :create , :destroy]
+    resources :attendances, only: [:index, :show, :destroy]
+  end
 
-  resources :events, only: [:index, :show, :new, :create , :destroy]
-
+  resources :events, only: [:index, :show]
 end
